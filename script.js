c03af39b3f01bb304646737565a016fad6db5bf3
gustavo444insano <gustavo.martins.gomes@escola.pr.gov.br>
2025-09-05 11:21:13 -0300

    mega update

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ function updateStory() {
 
   // Lógica da história com base no estado
   if (storyStage === 'start') {
-    description.innerText = "Você é um astronauta a bordo da nave espacial *Orion*. Após meses de viagem, você finalmente chega a um planeta desconhecido. O que deseja fazer?";
+    description.innerText = "Otho, o astronauta, acaba de chegar a um planeta desconhecido. Sua missão é explorar e descobrir os segredos que ele esconde. O que Otho vai fazer?";
     option1.innerText = "Explorar a superfície";
     option2.innerText = "Ficar na nave e monitorar o planeta";
   } 
   else if (storyStage === 'explore') {
-    description.innerText = "Você desce na superfície do planeta e encontra uma estrutura alienígena. Você se aproxima com cautela, mas uma porta se abre automaticamente à sua frente. O que você fará?";
+    description.innerText = "Otho desce na superfície do planeta e encontra uma estrutura alienígena. Ele se aproxima com cautela, mas uma porta se abre automaticamente à sua frente. O que Otho fará?";
     option1.innerText = "Entrar na estrutura alienígena";
     option2.innerText = "Retornar à nave";
   }
   else if (storyStage === 'alienStructure') {
-    description.innerText = "Dentro da estrutura, você encontra um artefato brilhante e misterioso. Ele começa a brilhar intensamente e você sente uma estranha sensação de conexão mental. Você decide...";
+    description.innerText = "Dentro da estrutura, Otho encontra um artefato brilhante e misterioso. Ele começa a brilhar intensamente e Otho sente uma estranha sensação de conexão mental. O que Otho fará?";
     option1.innerText = "Tocar o artefato";
     option2.innerText = "Deixar o artefato e voltar para a nave";
   }
   else if (storyStage === 'touchArtifact') {
-    description.innerText = "Ao tocar o artefato, você é transportado para outra dimensão, onde seres alienígenas começam a se comunicar com você mentalmente. Eles oferecem uma escolha: você pode retornar à Terra ou explorar mais o universo.";
+    description.innerText = "Ao tocar o artefato, Otho é transportado para outra dimensão, onde seres alienígenas começam a se comunicar com ele mentalmente. Eles oferecem uma escolha: Otho pode retornar à Terra ou explorar mais o universo.";
     option1.innerText = "Aceitar explorar mais o universo";
     option2.innerText = "Retornar para a Terra";
   }
   else if (storyStage === 'backToShip') {
-    description.innerText = "Você volta para a nave, decidido a continuar sua missão. O planeta continua sendo um mistério, mas você tem muitas mais aventuras pela frente.";
+    description.innerText = "Otho volta para a nave, decidido a continuar sua missão. O planeta continua sendo um mistério, mas Otho tem muitas mais aventuras pela frente.";
     option1.innerText = "Iniciar nova jornada";
     option2.innerText = "Fim da missão";
   } 
